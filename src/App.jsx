83d297eaa30bc20f40d05697a875f41e378a0e0a
Chaitanya <chaitanyagalla1813@gmail.com>
2025-04-11 22:22:22 +0530

    delete env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import {  Route, Routes, BrowserRouter } from "react-router-dom";
 import Doctor from './pages/Doctor';
 import DoctorDetails from './pages/DoctorDetails';
+import DoctorSignUp from './pages/DoctorSignUp';
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-            <Route path='/'  element={<Doctor/>} ></Route>
-            <Route path='/doctor/:doctorId' element= {<DoctorDetails/>} ></Route>
+            <Route path='/'  element = {<Doctor/>} ></Route>
+            <Route path='/doctor/:doctorId' element = {<DoctorDetails/>} ></Route>
+            <Route path='/signup' element = {<DoctorSignUp/>} />
         </Routes>
       </BrowserRouter>
 
