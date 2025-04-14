369fede9aac578f14eac7b248c2781411e849fae
Chaitanya <chaitanyagalla1813@gmail.com>
2025-04-14 12:00:55 +0530

    fix: fixed dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { StoreProvider } from "./store/Context.jsx"
+import { StoreProvider } from "./store/Context.js"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
